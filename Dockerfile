37f23decd716df04bdc04eca2ceb569ab3f771c8
Shakir_Hasan <sakirhasan133@gmail.com>
2024-11-28 21:29:29 +0600

    Docker File added

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+
+
+FROM node:22
+
+ENV NODE_ENV=production
+
+WORKDIR /app
+
+COPY ["package.json", "package-lock.json*", "./"]
+
+RUN npm install --production
+
+COPY . .
+
+EXPOSE 5000
+
+CMD [ "node", "index.js" ]
--- index.js
+++ index.js
@@ -6,13 +6,7 @@ const port = process.env.PORT || 5000;
 
 //Midleware
 
-app.use(
-  cors({
-    origin: [""],
-    methods: ["POST", "GET"],
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
